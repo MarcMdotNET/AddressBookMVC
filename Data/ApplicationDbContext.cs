afe15ad43dfad67eacb0d6639f02886d3fe9566e
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2022-10-15 16:30:14 -0400

    Functional Application
    The application is functional. Now it needs some polishing.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AddressBookMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AddressBookMVC.Data
 {
@@ -8,5 +9,7 @@ namespace AddressBookMVC.Data
         {
 
         }
+
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
